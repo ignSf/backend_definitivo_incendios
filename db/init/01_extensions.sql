510351c4ad324fe655faa6eaf2b970bb652b12d4
ignSf <ign.salazar97@gmail.com>
2026-05-06 04:53:48 -0400

    Delete db/init directory

--- db/init/01_extensions.sql
+++
@@ -1,8 +0,0 @@
--- ============================================================
--- 01_extensions.sql
--- Extensiones necesarias para el proyecto
--- ============================================================
-
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE EXTENSION IF NOT EXISTS "postgis";
-CREATE EXTENSION IF NOT EXISTS "pg_trgm";
